734a286e366b18df19b6cdb9a8e3eea7398752ba
Joshua Smith <joshua.smith@generalassemb.ly>
2022-11-29 10:58:51 -0700

    middle of lesson state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+// import Counter from './components/Counter'
+import Counter, {Example} from './components/Counter'
+
+// console.log(Counter)
+// Example()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* React State Lesson */}
+        {/* importing our state management tools, defining state, updating state, and observing state changes in our browser */}
+        <h1>Intro to State in React</h1>
+        < Counter />
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,89 @@
+import {useState} from 'react'
+
+// state is a tool imported from react library 
+    // when declaring state we declare our variables inside of our component's scope {}
+    // use array destructuring to access pieces of the imported state tools (hooks)
+    // stateful component is a component that monitors its own state rather than exclusively receiveing data through props 
+
+    // Example Syntax: 
+    // const stateTools = useState(10) // stateTools => [state, dispatcher]
+    
+    // react hook (useState) -> listening for changes to certain value
+    // useState() -> inside of the component is invoked calling the function (initial value for our variable)
+    // [initial value, dispatcher] 
+    // dispatcher(newValue) => react takes the new value and updates the state variable
+    // then immediate triggers a rerender of the component
+    // console.log(stateTools[0])
+
+
+
+function Counter(props) {
+
+    
+    let [counter,setCounter] = useState(10) 
+    const [history, setHistory] = useState([counter])
+    const [background, setBackground] = useState("even")
+    // counter === useState[0]
+    // setCounter === useState[1]
+
+    // array destructuring to assign values in an array to variables, based on their positions
+    // counter+=66 // -> a direct mutation of our value 
+    // setCounter(counter + 1) -> when you call your set function (dispatcher) -> it will update state with the provided value, and then rerender the component
+    // when a component is rendered -> Counter -> Counter()
+    console.log('I am rendering a counter')
+    
+    // Objective: 
+    // write a "handle decrement" function handler 
+    function handleDecrement (){
+        const newNumber = counter - 1
+        if(newNumber >=0 ){
+            setCounter(newNumber)
+            // setHistory([...history, newNumber])
+        } 
+    }
+    // - prevents the number from going below zero
+    // - when handler is called it will try to decrement counter by 1 (setCounter)
+    
+    function handleIncrement (){
+        const newNumber = counter + 1
+
+        if(newNumber > 20){
+            setCounter(10)
+            // setHistory([...history, 10])
+        } else {
+            setCounter(newNumber)
+            // setHistory([...history, newNumber])
+            // whatever value you pass to your set function(setCounter) =-> overrides + updates counter 
+        }
+        
+        // setCounter Dispatcher triggers reacts update behaviors
+    }
+
+    return (
+        <div className={ "Counter "+ background }>
+            <h3>Counter</h3>
+            {/* <h4>History: {`[ ${history.join(", ")}]`}</h4> */}
+            <p>Current Value: <span className="display-number">{counter}</span></p>
+            <section>
+                <button onClick={ handleDecrement }>-</button>
+                <button onClick={ handleIncrement } >+</button>
+            </section>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default Counter
+export function Example() {
+    console.log("This is a named export")
+}
